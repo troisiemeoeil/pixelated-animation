65fe65c6294027c4d94030170fe62910616b6acc
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-10-02 02:32:16 +0200

    add transition

--- scripts.js
+++ scripts.js
@@ -1,31 +1,24 @@
 
 
 // Code that runs on pageload
-// gsap.to(".load_grid-item", {
-//   opacity: 0,
-//   duration: 0.001,
-//   stagger: { amount: 0.5, from: "random" },
-//   onComplete: () => {
-//     gsap.set(".load_grid", { display: "none" });
-//   }
-// });
-gsap.set(".load_grid", { display: "grid" });
-gsap.to(
-  ".load_grid_item",
-  {
-    opacity: 0,
-    duration: 0.001,
-    stagger: { amount: 0.5, from: "random" }, //you can also try a from: "start" or "end" -- get creative!
-  },
-  
-);
+gsap.to(".load_grid_item", {
+  opacity: 0,
+  duration: 0.001,
+  stagger: { amount: 0.5, from: "random" },
+  onComplete: () => {
+    gsap.set(".load_grid", { display: "none" });
+  }
+});
 
 // Code that runs on click of a link
 $(document).ready(function () {
-  $("body").on("click", function (e) {
- 
+  $("a").on("click", function (e) {
+    if (
+    	$(this).prop("hostname") === window.location.host &&
+      $(this).attr("href").indexOf("#") === -1 &&
+      $(this).attr("target") !== "_blank") {
         e.preventDefault();
-        let destination = $("a").attr("href");
+        let destination = $(this).attr("href");
         gsap.set(".load_grid", { display: "grid" });
         gsap.fromTo(
           ".load_grid_item",
@@ -42,12 +35,12 @@ $(document).ready(function () {
           }
         );
     }
-  );
+  });
   
-  // // On click of the back button
-  // window.onpageshow = function(event){
-  // 	if (event.persisted) {
-  //   	window.location.reload();
-  //   }
-  // }
+  // On click of the back button
+  window.onpageshow = function(event){
+  	if (event.persisted) {
+    	window.location.reload();
+    }
+  }
 });
